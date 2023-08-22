9e239a065ee9f766fee624fa787f62ed8a180972
Manoko Langa <112900623+ManokoBranny@users.noreply.github.com>
2023-08-22 11:33:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 import numpy as np
 
 
-model = tf.keras.models.load_model("my_model.h5")  # Replace with the path to your model file
+model = tf.keras.models.load_model("loan.h5")  # Replace with the path to your model file
 
 
 st.title("Loan Approval Prediction")
