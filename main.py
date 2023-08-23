637be96dc2585e868fbcd9934b4712deec114a81
Manoko Langa <112900623+ManokoBranny@users.noreply.github.com>
2023-08-23 16:11:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 #model = tf.keras.models.load_model("loan.h5")  # Replace with the path to your model file
-model = tf.keras.models.load_model('manoko.h5')
+model = tf.keras.models.load_model('manoko2.h5')
 
 
 st.title("Loan Approval Prediction")
