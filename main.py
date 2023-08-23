2b62b478af2a88d790759c43072c10d1b228a9f0
Manoko Langa <112900623+ManokoBranny@users.noreply.github.com>
2023-08-23 16:22:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -43,7 +43,12 @@ if st.button("Predict"):
  
 
     # Preprocess input_data if needed (e.g., scaling, feature engineering)
-    input_data = input_data.reshape(1, -1)
+    # Standardize numerical features
+      scaler = StandardScaler()
+      X_train_scaled = scaler.fit_transform(X_train)
+      X_test_scaled = scaler.transform(X_test)
+     input_data = scaler.transform(input_data)
+    #input_data = input_data.reshape(1, -1)
 
     # Use the loaded model to make predictions
     prediction = model.predict(input_data)
