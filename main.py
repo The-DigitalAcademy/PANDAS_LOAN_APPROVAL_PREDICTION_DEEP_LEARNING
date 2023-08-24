cac9a4133cc6b601abbc9aad6101dd1011d7d05a
Manoko Langa <112900623+ManokoBranny@users.noreply.github.com>
2023-08-24 15:42:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -31,11 +31,11 @@ if st.button("Predict"):
     input_data = np.array([[no_of_dependents, income_annum,
                             loan_amount, loan_term, cibil_score, residential_assets_value,
                             commercial_assets_value, luxury_assets_value, bank_asset_value]])
-   inp = pd.Dataframe({}
+   inp = pd.Dataframe({
                      "no_of_dependents": [no_of_dependents],"income_annum": [income_annum],"loan_amount": [loan_amount],
                     "loan_term": [loan_term],"cibil_score": [cibil_score],"residential_assets_value": [residential_assets_value],
                     "commercial_assets_value": [commercial_assets_value],"luxury_assets_value": [luxury_assets_value],
-                    "bank_asset_value": [bank_asset_value],)
+                    "bank_asset_value": [bank_asset_value]})
 
     # Standardize the input data using the loaded scaler
     # input_data = scaler.fit_transform(input_data.reshape(1,-1))
