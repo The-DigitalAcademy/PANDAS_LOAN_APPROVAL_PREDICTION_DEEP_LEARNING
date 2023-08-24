b0d4cfd937995d947fdc3f3519b43624cde88801
Manoko Langa <112900623+ManokoBranny@users.noreply.github.com>
2023-08-24 15:46:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ if st.button("Predict"):
     input_data = np.array([[no_of_dependents, income_annum,
                             loan_amount, loan_term, cibil_score, residential_assets_value,
                             commercial_assets_value, luxury_assets_value, bank_asset_value]])
-    inp = pd.Dataframe({
+    inp = pd.DataFrame({
                     "no_of_dependents": [no_of_dependents],
                     "income_annum": [income_annum],
                     "loan_amount": [loan_amount],
