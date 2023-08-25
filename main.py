56421bf925b56e76774d030e7aa7b0bd768520f6
Manoko Langa <112900623+ManokoBranny@users.noreply.github.com>
2023-08-25 12:13:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import tensorflow as tf
 import numpy as np
 from sklearn.preprocessing import LabelEncoder, StandardScaler
+import joblib
 
 
 #model = tf.keras.models.load_model("loan.h5")  # Replace with the path to your model file
