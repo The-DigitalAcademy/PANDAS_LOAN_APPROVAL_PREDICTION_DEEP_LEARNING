f52e8f7b6434c0f674647d4675c835d578df9134
Manoko Langa <112900623+ManokoBranny@users.noreply.github.com>
2023-08-25 11:53:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
 import streamlit as st
 import tensorflow as tf
 import numpy as np
-import joblib
+from sklearn.preprocessing import LabelEncoder, StandardScaler
 
-# Function to load the model
-def load_model(model_name):
-    return tf.keras.models.load_model(model_name)
 
-# Load the class prediction model
-model1 = load_model('model1.h5')
+#model = tf.keras.models.load_model("loan.h5")  # Replace with the path to your model file
+model = tf.keras.models.load_model('manoko3.h5')
 
-# Load the probability prediction model
-model2 = load_model('model2.h5')
-
-# Load the scaler
-scaler = joblib.load('scaler3.pkl')
 
 st.title("Loan Approval Prediction")
 
-# Radio button to select the prediction model
-#selected_model = st.radio("Select Prediction Model", ["Class Prediction", "Probability Prediction"])
+import streamlit as st
+import numpy as np
 
-no_of_dependents = st.number_input("Number of Dependents:")
+# Sample input fields corresponding to the columns in your X_train dataset
+st.title("Loan Approval Prediction")
+
+#education=["Not Graduate", "Graduate"]
+#self_employed=["No", "Yes"]
+# Fit and transform categorical variables
+#education_encoded = education_encoder.fit_transform([education])
+#self_employed_encoded = self_employed_encoder.fit_transform([self_employed])
+
+
+no_of_dependents =st.number_input("Number of Dependents:")
+#education = st.selectbox("Education", ["Not Graduate", "Graduate"])
+#self_employed = st.selectbox("Self Employed", ["No", "Yes"])
 income_annum = st.number_input("Annual Income:")
 loan_amount = st.number_input("Loan Amount:")
 loan_term = st.number_input("Loan Term (in months):")
@@ -33,19 +37,22 @@ bank_asset_value = st.number_input("Bank Asset Value:")
 
 # Make predictions when a button is clicked
 if st.button("Predict"):
-    # Prepare the input data for prediction
+    # Prepare the input data for prediction (ensure it matches your model's input format)
     input_data = np.array([no_of_dependents, income_annum,
                             loan_amount, loan_term, cibil_score, residential_assets_value,
                             commercial_assets_value, luxury_assets_value, bank_asset_value])
+ 
 
-    # Standardize the input data using the loaded scaler
-    input_data = scaler.transform(input_data.reshape(1, -1))
+    # Preprocess input_data if needed (e.g., scaling, feature engineering)
+    # Standardize numerical features
+    scaler = StandardScaler()
+    #X_train_scaled = scaler.fit_transform(X_train)
+    #X_test_scaled = scaler.transform(X_test)
+    #input_data = scaler.transform(input_data)
+    input_data = input_data.reshape(1, -1)
 
-    # Use the selected model to make predictions
-    if selected_model == "Class Prediction":
-        prediction = predict(model1, input_data)
-        loan_approval_class = int(round(prediction[0, 0]))
-        st.write(f"Loan Approval Class: {loan_approval_class}")
-    elif selected_model == "Probability Prediction":
-        prediction = predict(model2, input_data)
-    
+    # Use the loaded model to make predictions
+    prediction = model.predict(input_data)
+
+    # Display the prediction
+    st.write(f"Loan Approval Probability: {prediction[0, 0]:.2%}")
