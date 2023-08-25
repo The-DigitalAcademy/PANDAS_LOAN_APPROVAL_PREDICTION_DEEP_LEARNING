8bb9940f31071230eb447102c94e3168cb060c1d
Manoko Langa <112900623+ManokoBranny@users.noreply.github.com>
2023-08-25 12:12:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
 import streamlit as st
 import tensorflow as tf
 import numpy as np
-import joblib
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder, StandardScaler
 
 
-# Load your trained model (replace with the path to your model file)
-model = tf.keras.models.load_model('model2.h5')
+#model = tf.keras.models.load_model("loan.h5")  # Replace with the path to your model file
+model = tf.keras.models.load_model('model1.h5')
 # Load the scaler
 scaler = joblib.load('scaler3.pkl')
+
+
+st.title("Loan Approval Prediction")
+
+import streamlit as st
+import numpy as np
 
 # Sample input fields corresponding to the columns in your X_train dataset
 st.title("Loan Approval Prediction")
 
-no_of_dependents = st.number_input("Number of Dependents:")
+#education=["Not Graduate", "Graduate"]
+#self_employed=["No", "Yes"]
+# Fit and transform categorical variables
+#education_encoded = education_encoder.fit_transform([education])
+#self_employed_encoded = self_employed_encoder.fit_transform([self_employed])
+
+
+no_of_dependents =st.number_input("Number of Dependents:")
+#education = st.selectbox("Education", ["Not Graduate", "Graduate"])
+#self_employed = st.selectbox("Self Employed", ["No", "Yes"])
 income_annum = st.number_input("Annual Income:")
 loan_amount = st.number_input("Loan Amount:")
 loan_term = st.number_input("Loan Term (in months):")
@@ -25,25 +39,21 @@ bank_asset_value = st.number_input("Bank Asset Value:")
 
 # Make predictions when a button is clicked
 if st.button("Predict"):
-    # Prepare the input data for prediction
+    # Prepare the input data for prediction (ensure it matches your model's input format)
     input_data = np.array([no_of_dependents, income_annum,
                             loan_amount, loan_term, cibil_score, residential_assets_value,
                             commercial_assets_value, luxury_assets_value, bank_asset_value])
+ 
 
-    # Standardize the input data using the same scaler used during training
-    # Load the scaler (replace with the path to your scaler file)
+    # Preprocess input_data if needed (e.g., scaling, feature engineering)
+    # Standardize numerical features
     scaler = StandardScaler()
-    scaler = scaler.fit(input_data)  # Fit the scaler on your training data
-    input_data = scaler.transform(input_data.reshape(1, -1))
-
+    input_data = scaler.fit_transform(input_data)
+    input_data = scaler.transform(input_data)
+    input_data = input_data.reshape(1, -1)
+x
     # Use the loaded model to make predictions
     prediction = model.predict(input_data)
 
-    # Determine the class (0 or 1) based on the prediction
-    loan_approval_class = 1 if prediction[0, 0] >= 0.5 else 0
-
-    # Map the class to "not approved" or "approved"
-    approval_status = "approved" if loan_approval_class == 1 else "not approved"
-
-    # Display the loan approval status
-    st.write(f"Loan Approval Status: {approval_status}")
+    # Display the prediction
+    st.write(f"Loan Approval Probability: {prediction[0, 0]}")
