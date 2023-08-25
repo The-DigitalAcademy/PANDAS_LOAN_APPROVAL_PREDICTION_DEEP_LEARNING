8f6c06242abc8f3839157996aff2e72a2187aa75
Manoko Langa <112900623+ManokoBranny@users.noreply.github.com>
2023-08-25 11:34:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import streamlit as st
 import tensorflow as tf
 import numpy as np
-from sklearn.preprocessing import StandardScaler
 import joblib
-import pandas as pd
 
-# Load the model
-model = tf.keras.models.load_model('manoko6.h5')
+# Load the class prediction model
+model1 = tf.keras.models.load_model('model1.h5')
 
-# Load the StandardScaler fitted to your training data
-scaler = joblib.load('scaler2.pkl')  # Replace with the path to your scaler file
+# Load the probability prediction model
+model2 = tf.keras.models.load_model('model2.h5')
+
+# Load the scaler
+scaler = joblib.load('scaler3.pkl')
 
 st.title("Loan Approval Prediction")
 
@@ -26,35 +27,24 @@ commercial_assets_value = st.number_input("Commercial Assets Value:")
 luxury_assets_value = st.number_input("Luxury Assets Value:")
 bank_asset_value = st.number_input("Bank Asset Value:")
 
+# Radio button to select the prediction model
+selected_model = st.radio("Select Prediction Model", ["Class Prediction", "Probability Prediction"])
+
 # Make predictions when a button is clicked
 if st.button("Predict"):
     # Prepare the input data for prediction
-    input_data = np.array([[no_of_dependents, income_annum,
+    input_data = np.array([no_of_dependents, income_annum,
                             loan_amount, loan_term, cibil_score, residential_assets_value,
-                            commercial_assets_value, luxury_assets_value, bank_asset_value]])
-    inp = pd.DataFrame({
-                    "no_of_dependents": [no_of_dependents],
-                    "income_annum": [income_annum],
-                    "loan_amount": [loan_amount],
-                    "loan_term": [loan_term],
-                    "cibil_score": [cibil_score],
-                    "residential_assets_value": [residential_assets_value],
-                    "commercial_assets_value": [commercial_assets_value],
-                    "luxury_assets_value": [luxury_assets_value],
-                    "bank_asset_value": [bank_asset_value]
-                    })
+                            commercial_assets_value, luxury_assets_value, bank_asset_value])
 
     # Standardize the input data using the loaded scaler
-    # input_data = scaler.fit_transform(input_data.reshape(1,-1))
-    
+    input_data = scaler.transform(input_data.reshape(1, -1))
 
-    # Use the loaded model to make predictions
-    prediction = model.predict(inp)
-    # p = scaler.fit_transform(prediction.reshape(1,-1))
-
-    # Predicted class (0 or 1)
-    loan_approval_class = np.argmax(prediction)
-
-    # Display the prediction class and probability as a percentage
-    st.write(f"Loan Approval Class: {prediction}")
-    st.write(f"Loan Approval Probability: {p[0, loan_approval_class] * 100:.2f}%")
+    # Use the selected model to make predictions
+    if selected_model == "Class Prediction":
+        prediction = model1.predict(input_data)
+        loan_approval_class = int(round(prediction[0, 0]))
+        st.write(f"Loan Approval Class: {loan_approval_class}")
+    elif selected_model == "Probability Prediction":
+        prediction = model2.predict(input_data)
+        st.write(f"Loan Approval Probability: {prediction[0, 0] * 100:.2f}%")
