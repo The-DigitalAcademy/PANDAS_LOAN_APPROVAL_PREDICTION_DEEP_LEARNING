4968991050a567ca34157d0c8415bf0a08275ce6
Manoko Langa <112900623+ManokoBranny@users.noreply.github.com>
2023-08-28 15:29:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -25,6 +25,14 @@ def main():
     st.title("Loan Approval Prediction")
 
     no_of_dependents = st.number_input("Number of Dependents", min_value=0, max_value=10, step=1)
+    income_annum = st.number_input("Annual Income", min_value=0.0, max_value=1000000.0, step=1000.0)
+    loan_amount = st.number_input("Loan Amount", min_value=0.0, max_value=1000000.0, step=1000.0)
+    loan_term = st.number_input("Loan Term (in months)", min_value=1, max_value=360, step=1)
+    cibil_score = st.number_input("CIBIL Score", min_value=0, max_value=1000, step=1)
+    residential_assets_value = st.number_input("Residential Assets Value", min_value=0.0, max_value=1000000.0, step=1000.0)
+    commercial_assets_value = st.number_input("Commercial Assets Value", min_value=0.0, max_value=1000000.0, step=1000.0)
+    luxury_assets_value = st.number_input("Luxury Assets Value", min_value=0.0, max_value=1000000.0, step=1000.0)
+    bank_asset_value = st.number_input("Bank Asset Value", min_value=0.0, max_value=1000000.0, step=1000.0)
     # ...
 
     # Make predictions when a button is clicked
