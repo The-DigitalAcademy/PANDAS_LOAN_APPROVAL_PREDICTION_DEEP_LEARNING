051af61e6c8164321a133ed719e1d1bb81a4b1f6
Manoko Langa <112900623+ManokoBranny@users.noreply.github.com>
2023-08-28 15:10:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,58 +1,50 @@
 import streamlit as st
 import tensorflow as tf
 import numpy as np
-from sklearn.preprocessing import LabelEncoder, StandardScaler
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
 
-
-#model = tf.keras.models.load_model("loan.h5")  # Replace with the path to your model file
-model = tf.keras.models.load_model('manoko3.h5')
-
-
-st.title("Loan Approval Prediction")
-
-import streamlit as st
-import numpy as np
+# Load your trained model (replace with the path to your model file)
+model = tf.keras.models.load_model('final.h5')
 
 # Sample input fields corresponding to the columns in your X_train dataset
 st.title("Loan Approval Prediction")
 
-#education=["Not Graduate", "Graduate"]
-#self_employed=["No", "Yes"]
-# Fit and transform categorical variables
-#education_encoded = education_encoder.fit_transform([education])
-#self_employed_encoded = self_employed_encoder.fit_transform([self_employed])
-
-
-no_of_dependents =st.number_input("Number of Dependents:")
-#education = st.selectbox("Education", ["Not Graduate", "Graduate"])
-#self_employed = st.selectbox("Self Employed", ["No", "Yes"])
-income_annum = st.number_input("Annual Income:")
-loan_amount = st.number_input("Loan Amount:")
-loan_term = st.number_input("Loan Term (in months):")
-cibil_score = st.number_input("CIBIL Score:")
-residential_assets_value = st.number_input("Residential Assets Value:")
-commercial_assets_value = st.number_input("Commercial Assets Value:")
-luxury_assets_value = st.number_input("Luxury Assets Value:")
-bank_asset_value = st.number_input("Bank Asset Value:")
+no_of_dependents = st.number_input("Number of Dependents", min_value=0, max_value=10, step=1)
+income_annum = st.number_input("Annual Income", min_value=0.0, max_value=1000000.0, step=1000.0)
+loan_amount = st.number_input("Loan Amount", min_value=0.0, max_value=1000000.0, step=1000.0)
+loan_term = st.number_input("Loan Term (in months)", min_value=1, max_value=360, step=1)
+cibil_score = st.number_input("CIBIL Score", min_value=0, max_value=1000, step=1)
+residential_assets_value = st.number_input("Residential Assets Value", min_value=0.0, max_value=1000000.0, step=1000.0)
+commercial_assets_value = st.number_input("Commercial Assets Value", min_value=0.0, max_value=1000000.0, step=1000.0)
+luxury_assets_value = st.number_input("Luxury Assets Value", min_value=0.0, max_value=1000000.0, step=1000.0)
+bank_asset_value = st.number_input("Bank Asset Value", min_value=0.0, max_value=1000000.0, step=1000.0)
 
 # Make predictions when a button is clicked
 if st.button("Predict"):
-    # Prepare the input data for prediction (ensure it matches your model's input format)
-    input_data = np.array([no_of_dependents, income_annum,
-                            loan_amount, loan_term, cibil_score, residential_assets_value,
-                            commercial_assets_value, luxury_assets_value, bank_asset_value])
- 
+    # Prepare the input data for prediction
+    input_data = np.array([no_of_dependents, income_annum, loan_amount, loan_term, cibil_score,
+                            residential_assets_value, commercial_assets_value, luxury_assets_value, bank_asset_value])
 
-    # Preprocess input_data if needed (e.g., scaling, feature engineering)
-    # Standardize numerical features
+    # Standardize the input data using the same scaler used during training
+    # Load the scaler (replace with the path to your scaler file)
     scaler = StandardScaler()
-    #X_train_scaled = scaler.fit_transform(X_train)
-    #X_test_scaled = scaler.transform(X_test)
-    #input_data = scaler.transform(input_data)
-    input_data = input_data.reshape(1, -1)
+    scaler = scaler.fit(X_train)  # Fit the scaler on your training data
+    input_data = scaler.transform(input_data.reshape(1, -1))
 
     # Use the loaded model to make predictions
     prediction = model.predict(input_data)
 
-    # Display the prediction
-    st.write(f"Loan Approval Probability: {prediction[0, 0]")
+    # Create a bar chart to visualize the prediction
+    plt.bar([0, 1], prediction[0], tick_label=['Rejected (0)', 'Approved (1)'])
+    plt.xlabel('Loan Status')
+    plt.ylabel('Probability')
+    plt.title('Loan Approval Probability Distribution')
+    st.pyplot(plt)
+
+    # Print the predicted class
+    predicted_class = np.argmax(prediction)
+    if predicted_class == 0:
+        st.write("Prediction: Rejected (Class 0)")
+    else:
+        st.write("Prediction: Approved (Class 1)")
