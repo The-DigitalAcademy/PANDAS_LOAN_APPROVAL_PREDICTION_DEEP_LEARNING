c6bcd2f1ed7f5f07c0f31f9ae8220c544a12a0b3
Manoko Langa <112900623+ManokoBranny@users.noreply.github.com>
2023-08-29 15:57:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,13 @@ import tensorflow as tf
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
+import joblibimport streamlit as st
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
 import joblib
+from PIL import Image
 
 # Load your trained model (replace with the path to your model file)
 model = tf.keras.models.load_model('final4.h5')
@@ -15,41 +21,57 @@ scaler = joblib.load('second_last_scaler.pkl')
 # Define column names in the same order as your training data
 columns = ['no_of_dependents', 'income_annum', 'loan_amount', 'loan_term', 'cibil_score']
 # Add more column names as needed to match your training data
-def main():
 
+# Define a function to display the "Meet the Team" page
+def meet_the_team():
+    st.title("Meet the Team")
     
-# Set page configuration and title
-     #st.set_page_config(page_title="Loan Approval Prediction", layout="wide")
-    # Sample input fields corresponding to the columns in your training data
-    st.title("Loan Approval Prediction")
+    # Add team members with their pictures and descriptions
+    team_members = [
+        {"name": "John Doe", "position": "Data Scientist", "image": "john_doe.jpg", "description": "John is a data scientist with expertise in machine learning and data analysis."},
+        {"name": "Jane Smith", "position": "Web Developer", "image": "jane_smith.jpg", "description": "Jane is a web developer responsible for creating the Streamlit app."},
+        # Add more team members as needed
+    ]
+    
+    for member in team_members:
+        st.write(f"## {member['name']}")
+        st.image(member['image'], caption=member['name'], use_column_width=True)
+        st.write(f"**Position**: {member['position']}")
+        st.write(member['description'])
 
+# Set page configuration and title
+st.title("Loan Approval Prediction")
+
+# Sidebar
+with st.sidebar:
+    # Add an option in the sidebar to navigate to the "Meet the Team" page
+    page_selection = st.selectbox("Navigation", 
+                                   ["Loan Approval Prediction", "Meet the Team"])
+    if page_selection == "Meet the Team":
+        meet_the_team()
+
+# Main content
+if page_selection == "Loan Approval Prediction":
     # Create a DataFrame from the input variables
     input_df = pd.DataFrame(columns=columns)
     input_df.loc[0] = [0] * len(columns)  # Initialize with zeros, you can replace these with your desired default values
-   # Create input fields for each column
+    
+    # Create input fields for each column
     for column in columns:
         input_df[column] = st.number_input(f"{column.replace('_', ' ').title()}", value=input_df[column].values[0])
+    
     # Make predictions when a button is clicked
     if st.button("Predict"):
         # Standardize the input data using the loaded scaler
         input_data = input_df.values  # Convert DataFrame to array
         input_data = scaler.transform(input_data)
+        
         # Use the loaded model to make predictions
         prediction = model.predict(input_data)
-        #prediction = predict(model, input_data)
-         # Print the predicted class
-        predicted_class = np.argmax(prediction)
         result = "likely to be approved" if prediction >= 0.5 else "not likely to be approved"
+        
         st.write(f"Prediction: {prediction[0]}")
-        st.write(f"Prediction: The client is {result}.")
-# Print the predicted class
-        predicted_class = np.argmax(prediction)
-        #st.markdown(predicted_class)
-        #st.write("prediction:ouput")
-        # if predicted_class == 0:
-        #     st.write("Prediction: Rejected (Class 0)")
-        # else:
-        #     st.write("Prediction: Approved (Class 1)")
+        st.write(f"The client is {result}.")
 
 if __name__ == '__main__':
     main()
