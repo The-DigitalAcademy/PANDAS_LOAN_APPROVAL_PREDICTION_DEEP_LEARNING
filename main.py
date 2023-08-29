045ac62e4ca6e628f8dd84324713b5ea9171b6b2
Manoko Langa <112900623+ManokoBranny@users.noreply.github.com>
2023-08-29 15:23:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ model = tf.keras.models.load_model('final4.h5')
 
 # Load your scaler (replace with the path to your scaler file)
 scaler = joblib.load('second_last_scaler.pkl')
+
 # Define column names in the same order as your training data
 columns = ['no_of_dependents', 'income_annum', 'loan_amount', 'loan_term', 'cibil_score']
 # Add more column names as needed to match your training data
-
 def main():
     # Sample input fields corresponding to the columns in your training data
     st.title("Loan Approval Prediction")
@@ -22,53 +22,30 @@ def main():
     # Create a DataFrame from the input variables
     input_df = pd.DataFrame(columns=columns)
     input_df.loc[0] = [0] * len(columns)  # Initialize with zeros, you can replace these with your desired default values
-
-    # Create input fields for each column
+   # Create input fields for each column
     for column in columns:
         input_df[column] = st.number_input(f"{column.replace('_', ' ').title()}", value=input_df[column].values[0])
-
     # Make predictions when a button is clicked
-    
     if st.button("Predict"):
-        try:
-            with st.spinner("Predicting..."):
-                # Standardize the input data using the loaded scaler
-                input_data = input_df.values
-                input_data = scaler.transform(input_data)
-
-                # Use the loaded model to make predictions
-                prediction = model.predict(input_data)
-                 # Display prediction result
-                predicted_class = np.argmax(prediction)
-                result = "likely to be approved" if prediction >= 0.5 else "not likely to be approved"
-                st.write(f"Prediction: {prediction[0]}")
-                st.write(f"Prediction: The client is {result}.")
-                predicted_class = np.argmax(prediction)
-if __name__ == '__main__':
-    main()
-                
-                
-    #if st.button("Predict"):
         # Standardize the input data using the loaded scaler
-               #input_data = input_df.values  # Convert DataFrame to array
-               #input_data = scaler.transform(input_data)
-
+        input_data = input_df.values  # Convert DataFrame to array
+        input_data = scaler.transform(input_data)
         # Use the loaded model to make predictions
-               #prediction = model.predict(input_data)
+        prediction = model.predict(input_data)
         #prediction = predict(model, input_data)
          # Print the predicted class
-               #predicted_class = np.argmax(prediction)
-               # result = "likely to be approved" if prediction >= 0.5 else "not likely to be approved"
-               # st.write(f"Prediction: {prediction[0]}")
-               # st.write(f"Prediction: The client is {result}.")
-
-        # Print the predicted class
-               #predicted_class = np.argmax(prediction)
+        predicted_class = np.argmax(prediction)
+        result = "likely to be approved" if prediction >= 0.5 else "not likely to be approved"
+        st.write(f"Prediction: {prediction[0]}")
+        st.write(f"Prediction: The client is {result}.")
+# Print the predicted class
+        predicted_class = np.argmax(prediction)
         #st.markdown(predicted_class)
         #st.write("prediction:ouput")
         # if predicted_class == 0:
         #     st.write("Prediction: Rejected (Class 0)")
         # else:
         #     st.write("Prediction: Approved (Class 1)")
-# if __name__ == '__main__':
-#     main()
+
+if __name__ == '__main__':
+    main()
