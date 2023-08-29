232dcfa4e5f2791f1118ab8c9c16148f17475034
Manoko Langa <112900623+ManokoBranny@users.noreply.github.com>
2023-08-29 10:18:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def main():
         input_data = scaler.transform(input_data)
 
         # Use the loaded model to make predictions
-        prediction = predict(model, input_data)
+        prediction = model.predict(input_data)
 
         # Print the predicted class
         predicted_class = np.argmax(prediction)
