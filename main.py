faaffcf1625df3a108d6ac9cdfbf3aa583623e2e
Manoko Langa <112900623+ManokoBranny@users.noreply.github.com>
2023-08-29 11:22:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -38,10 +38,11 @@ def main():
 
         # Print the predicted class
         predicted_class = np.argmax(prediction)
-        if predicted_class == 0:
-            st.write("Prediction: Rejected (Class 0)")
-        else:
-            st.write("Prediction: Approved (Class 1)")
+        st.write("prediction:ouput")
+        # if predicted_class == 0:
+        #     st.write("Prediction: Rejected (Class 0)")
+        # else:
+        #     st.write("Prediction: Approved (Class 1)")
 
 if __name__ == '__main__':
     main()
