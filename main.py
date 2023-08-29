606e6cb886b9c9a1b4223820307e402d4dba6671
Manoko Langa <112900623+ManokoBranny@users.noreply.github.com>
2023-08-29 09:45:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,28 +11,27 @@ model = tf.keras.models.load_model('final3.h5')
 # Load your scaler (replace with the path to your scaler file)
 scaler = joblib.load('last_scaler.pkl')
 
+# Define column names in the same order as your training data
+columns = ['no_of_dependents', 'income_annum', 'loan_amount', 'loan_term', 'cibil_score']
+# Add more column names as needed to match your training data
+
 def main():
     # Sample input fields corresponding to the columns in your training data
     st.title("Loan Approval Prediction")
 
-    no_of_dependents = st.number_input("Number of Dependents", min_value=0, max_value=10, step=1)
-    income_annum = st.number_input("Annual Income", min_value=0.0)
-    loan_amount = st.number_input("Loan Amount", min_value=0.0)
-    loan_term = st.number_input("Loan Term (in months)", min_value=1, max_value=360, step=1)
-    cibil_score = st.number_input("CIBIL Score", min_value=0, max_value=1000, step=1)
-    #residential_assets_value = st.number_input("Residential Assets Value", min_value=0.0, max_value=1000000.0, step=1000.0)
-    #commercial_assets_value = st.number_input("Commercial Assets Value", min_value=0.0, max_value=1000000.0, step=1000.0)
-    #luxury_assets_value = st.number_input("Luxury Assets Value", min_value=0.0, max_value=1000000.0, step=1000.0)
-    #bank_asset_value = st.number_input("Bank Asset Value", min_value=0.0, max_value=1000000.0, step=1000.0)
-    # ...
+    # Create a DataFrame from the input variables
+    input_df = pd.DataFrame(columns=columns)
+    input_df.loc[0] = [0] * len(columns)  # Initialize with zeros, you can replace these with your desired default values
+
+    # Create input fields for each column
+    for column in columns:
+        input_df[column] = st.number_input(f"{column.replace('_', ' ').title()}", value=input_df[column].values[0])
 
     # Make predictions when a button is clicked
     if st.button("Predict"):
-        # Prepare the input data for prediction
-        input_data = np.array([no_of_dependents, income_annum, loan_amount, loan_term, cibil_score])
-
         # Standardize the input data using the loaded scaler
-        input_data = scaler.transform(input_data.reshape(1, -1))
+        input_data = input_df.values  # Convert DataFrame to array
+        input_data = scaler.transform(input_data)
 
         # Use the loaded model to make predictions
         prediction = model.predict(input_data)
