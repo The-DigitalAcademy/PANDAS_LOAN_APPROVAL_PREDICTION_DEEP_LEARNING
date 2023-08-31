0c4e4c08fb7994798169344e5f98f6ac0239f7eb
Manoko Langa <112900623+ManokoBranny@users.noreply.github.com>
2023-08-31 09:17:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -56,6 +56,7 @@ def project_overview():
     # Display an image from the same directory as your script
     #st.image('https://github.com/The-DigitalAcademy/PANDAS_LOAN_APPROVAL_PREDICTION_DEEP_LEARNING/blob/main/loan-icon-.png')
     st.image('https://github.com/The-DigitalAcademy/PANDAS_LOAN_APPROVAL_PREDICTION_DEEP_LEARNING/blob/main/loanimage.jpeg')
+    st.image('/Users/da_m1_23/Downloads/Manoko-loan-default-prediction/loanimage.jpeg')
     #st.image(" st.image('/Users/da_m1_23/Downloads/deep_learning/loan-icon.jpeg')")
     st.write("This project is aimed at predicting loan approval using machine learning.")
     st.write("It uses a deep learning model to predict whether a loan application is likely to be approved or not.")
